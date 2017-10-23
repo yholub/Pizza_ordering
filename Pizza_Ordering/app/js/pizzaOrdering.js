18fe04a260fecac0d36c8a5bd509ff72ca663024
Sofiya <sofiya.padus@eleks.com>
2017-10-24 02:59:50 +0300

    Change pizza ordering page

--- Pizza_Ordering/app/js/pizzaOrdering.js
+++ Pizza_Ordering/app/js/pizzaOrdering.js
@@ -1,28 +1,25 @@
 ﻿$(function () {
-
+    var pizzasArr = [
+           { id: 0, imgUrl: "../../assets/images/food_icon03.jpg", name: "Основа", price: 20, ingredients: [] },
+           { id: 1, imgUrl: "../../assets/images/img1.png",        name: "Цезаре", price: 120, ingredients: ["моцарела", "айсберг", "пармезан", "курка"] },
+           { id: 2, imgUrl: "../../assets/images/food_icon02.jpg", name: "Вегетеріанська", price: 80, ingredients: ["пармезан", "рукола", "спаржа", "базилік"] },
+           { id: 3, imgUrl: "../../assets/images/food_icon04.jpg", name: "Маргарита", price: 55, ingredients: ["моцарела", "базилік", "помідори"] },
+           { id: 4, imgUrl: "../../assets/images/food_icon05.jpg", name: "Гавайська", price: 85, ingredients: ["моцарела", "ананаси", "кукурудза", "курка"] },
+           { id: 5, imgUrl: "../../assets/images/food_icon06.jpg", name: "Українська", price: 110, ingredients: ["моцарела", "оливки", "кукурудза", "шинка", "гриби", "помідори"] },
+           { id: 6, imgUrl: "../../assets/images/food_icon07.jpg", name: "Капрічоза", price: 95, ingredients: ["моцарела", "гриби", "оливки", "шинка"] }
+    ];
     var viewModel = {
-        pizzas: [
-           { imgUrl: "../../assets/images/img1.png", name: "pizza 1" },
-           { imgUrl: "../../assets/images/food_icon02.jpg", name: "pizza 2" },
-           { imgUrl: "../../assets/images/food_icon03.jpg", name: "pizza 3" },
-           { imgUrl: "../../assets/images/food_icon04.jpg", name: "pizza 4" },
-           { imgUrl: "../../assets/images/food_icon05.jpg", name: "pizza 5" },
-           { imgUrl: "../../assets/images/food_icon06.jpg", name: "pizza 6" },
-           { imgUrl: "../../assets/images/food_icon07.jpg", name: "pizza 7" }
-        ],
-        selectedProducts: [
-           { imgUrl: "../../assets/images/food_icon01.jpg", name: "pizza 1" },
-           { imgUrl: "../../assets/images/food_icon02.jpg", name: "pizza 2" },
-           { imgUrl: "../../assets/images/food_icon03.jpg", name: "pizza 3" },
-           { imgUrl: "../../assets/images/food_icon04.jpg", name: "pizza 4" },
-           { imgUrl: "../../assets/images/food_icon05.jpg", name: "pizza 5" }
-        ],
-        productCount: 5,
+        pizzas: ko.observableArray(pizzasArr),
+        selectedProducts: ko.observableArray([]),
         addToCard: function (data, event) {
-            alert(event.target.id);
+            var pizzaId = event.target.id;
+            console.log(pizzasArr);
+            var pizza = pizzasArr[Number(pizzaId)];
+            this.selectedProducts.push({ name: pizza.name, price: pizza.price, ingredients: pizza.ingredients });
         }
 
-    }
+
+    };
 
 
 
