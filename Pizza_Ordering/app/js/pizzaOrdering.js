7e749a60e3165508c6e005e79f4edc1e9a5650b0
Sofiya <sofiya.padus@eleks.com>
2017-10-24 01:11:48 +0300

    1) add pizza ordering page 2) add knockout 3) work on design for pizza ordering

---
+++ Pizza_Ordering/app/js/pizzaOrdering.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+    var viewModel = {
+        pizzas: [
+           { imgUrl: "../../assets/images/img1.png", name: "pizza 1" },
+           { imgUrl: "../../assets/images/food_icon02.jpg", name: "pizza 2" },
+           { imgUrl: "../../assets/images/food_icon03.jpg", name: "pizza 3" },
+           { imgUrl: "../../assets/images/food_icon04.jpg", name: "pizza 4" },
+           { imgUrl: "../../assets/images/food_icon05.jpg", name: "pizza 5" },
+           { imgUrl: "../../assets/images/food_icon06.jpg", name: "pizza 6" },
+           { imgUrl: "../../assets/images/food_icon07.jpg", name: "pizza 7" }
+        ],
+        selectedProducts: [
+           { imgUrl: "../../assets/images/food_icon01.jpg", name: "pizza 1" },
+           { imgUrl: "../../assets/images/food_icon02.jpg", name: "pizza 2" },
+           { imgUrl: "../../assets/images/food_icon03.jpg", name: "pizza 3" },
+           { imgUrl: "../../assets/images/food_icon04.jpg", name: "pizza 4" },
+           { imgUrl: "../../assets/images/food_icon05.jpg", name: "pizza 5" }
+        ],
+        productCount: 5,
+        addToCard: function (data, event) {
+            alert(event.target.id);
+        }
+
+    }
+
+
+
+    ko.applyBindings(viewModel);
+
+});
+
+
