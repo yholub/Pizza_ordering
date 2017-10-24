e187c24247704e19cf98109b8759cd370b19fdac
fakvarl <skolandrik@gmail.com>
2017-10-24 11:46:00 +0300

    temp moder

---
+++ Pizza_Ordering.Services/Interfaces/IOrderBL.cs
@@ -0,0 +1,21 @@
+﻿using Pizza_Ordering.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Ordering.Services.Interfaces
+{
+    public interface IOrderBL
+    {
+       List<Order> GetOrdersSince(DateTime st);
+       List<Order> GetPendingOrdersSince(DateTime st);
+      
+       void Accept(int id);
+
+
+       void Reject(int id);
+      
+    }
+}
---
+++ Pizza_Ordering/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using Pizza_Ordering.Models;
+using Pizza_Ordering.Services.BLs;
+using Pizza_Ordering.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pizza_Ordering.Controllers
+{
+   
+    public class OrderController : BaseController
+    {
+        private IOrderBL _service;
+        public OrderController(IOrderBL service)
+        {
+            _service = service;
+        }
+
+        [Route("api/order/GetCurrent")]
+        [HttpGet]
+        public IHttpActionResult GetCurrent()
+        {
+            DateTime now = DateTime.Now;
+            int min = DateTime.Now.Minute;
+            DateTime start = now - TimeSpan.FromMinutes(min / 10 * 10 + 10);
+            return Json(_service.GetOrdersSince(start).Select(o => new OrderViewModel(o)).ToList());
+        }
+
+        [Route("api/order/GetNew")]
+        [HttpGet]
+        public IHttpActionResult GetNew()
+        {
+            DateTime now = DateTime.Now;
+            int min = DateTime.Now.Minute;
+            DateTime start = now - TimeSpan.FromMinutes(min / 10 * 10 + 10);
+            return Json(_service.GetPendingOrdersSince(start).Select(o => new OrderViewModel(o)).ToList());
+        }
+
+        [Route("{id:long}")]
+        [HttpPost]
+        public void Accept(int id)
+        {
+
+            _service.Accept(id);
+        }
+
+        [Route("{id:long}")]
+        [HttpPost]
+        public void Reject(int id)
+        {
+            _service.Reject(id);
+        }
+
+    }
+}
