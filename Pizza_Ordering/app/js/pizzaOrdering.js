937cc85251f680df34741b68482566aad96a679e
Sofiya <sofiya.padus@eleks.com>
2017-10-24 14:57:38 +0300

    change pizza ordering page

--- Pizza_Ordering/app/js/pizzaOrdering.js
+++ Pizza_Ordering/app/js/pizzaOrdering.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    var pizzasArr = [
+    /*var pizzasArr = [
            { id: 0, imgUrl: "../../assets/images/food_icon03.jpg", name: "Основа", price: 20, ingredients: [] },
            { id: 1, imgUrl: "../../assets/images/img1.png",        name: "Цезаре", price: 120, ingredients: ["моцарела", "айсберг", "пармезан", "курка"] },
            { id: 2, imgUrl: "../../assets/images/food_icon02.jpg", name: "Вегетеріанська", price: 80, ingredients: ["пармезан", "рукола", "спаржа", "базилік"] },
@@ -22,8 +22,30 @@
     };
 
 
+    ko.applyBindings(viewModel);*/
 
-    ko.applyBindings(viewModel);
+    function ViewModel() {
+        var self = this;
+
+        self.pizzas = [
+           { id: 0, imgUrl: "../../assets/images/food_icon01.jpg", name: "Основа", price: 20, ingredients: [] },
+           { id: 1, imgUrl: "../../assets/images/zesare.jpeg", name: "Цезаре", price: 120, ingredients: ["моцарела", "айсберг", "пармезан", "курка"] },
+           { id: 2, imgUrl: "../../assets/images/veg.jpg", name: "Вегетеріанська", price: 80, ingredients: ["пармезан", "рукола", "спаржа", "базилік"] },
+           { id: 3, imgUrl: "../../assets/images/margarita.jpg", name: "Маргарита", price: 55, ingredients: ["моцарела", "базилік", "помідори"] },
+           { id: 4, imgUrl: "../../assets/images/havai.jpg", name: "Гавайська", price: 85, ingredients: ["моцарела", "ананаси", "кукурудза", "курка"] },
+           { id: 5, imgUrl: "../../assets/images/ukr.png", name: "Українська", price: 110, ingredients: ["моцарела", "оливки", "кукурудза", "шинка", "гриби", "помідори"] },
+           { id: 6, imgUrl: "../../assets/images/capri.jpg", name: "Капрічоза", price: 95, ingredients: ["моцарела", "гриби", "оливки", "шинка"] }
+        ];
+
+        self.selectedProducts = ko.observableArray([]),
+        self.addToCard = function (data, event) {
+            var pizzaId = Number(event.target.id);
+            var pizza = self.pizzas[pizzaId];
+            self.selectedProducts.push({ name: pizza.name, price: pizza.price, ingredients: pizza.ingredients });
+        }
+    }
+
+    ko.applyBindings(new ViewModel());
 
 });
 
