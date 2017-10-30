f65cd40e814599518241535cbe82b519b2308e85
Sofiya <sofiya.padus@eleks.com>
2017-10-30 11:08:06 +0200

    Change pizza Ordering page: Ingredients showing

--- Pizza_Ordering/app/js/user/pizzaOrdering.js
+++ Pizza_Ordering/app/js/user/pizzaOrdering.js
@@ -35,18 +35,6 @@
 
         self.pizzas = pizzas;
 
-        /*self.pizzas = [
-           { id: 0, imgUrl: "../../assets/images/food_icon01.jpg", name: "Основа", price: 20, ingredients: [] },
-           { id: 1, imgUrl: "../../assets/images/zesare.jpeg", name: "Цезаре", price: 120, ingredients: ["моцарела", "айсберг", "пармезан", "курка"] },
-           { id: 2, imgUrl: "../../assets/images/veg.jpg", name: "Вегетеріанська", price: 80, ingredients: ["пармезан", "рукола", "спаржа", "базилік"] },
-           { id: 3, imgUrl: "../../assets/images/margarita.jpg", name: "Маргарита", price: 55, ingredients: ["моцарела", "базилік", "помідори"] },
-           { id: 4, imgUrl: "../../assets/images/havai.jpg", name: "Гавайська", price: 85, ingredients: ["моцарела", "ананаси", "кукурудза", "курка"] },
-           { id: 5, imgUrl: "../../assets/images/ukr.png", name: "Українська", price: 110, ingredients: ["моцарела", "оливки", "кукурудза", "шинка", "гриби", "помідори"] },
-           { id: 6, imgUrl: "../../assets/images/capri.jpg", name: "Капрічоза", price: 95, ingredients: ["моцарела", "гриби", "оливки", "шинка"] }
-        ];*/
-        console.log(self.pizzas);
-
-
         self.selectedProducts = ko.observableArray([]),
         self.addToCard = function (data, event) {
             var pizzaId = Number(event.target.id);
